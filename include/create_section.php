e9bb683479d44482f7cab4032ab34f204824b577
GagaracruzJanbres <j.gagaracruz@yahoo.com>
2021-03-15 22:38:11 +0800

    Create create_section.php

---
+++ include/create_section.php
@@ -0,0 +1,35 @@
+<?php
+    include("database.php");
+
+    //create a new semester
+    if(isset($_POST['create'])){ 
+        $section = $_POST['section'];
+        $sql = "INSERT INTO section (sec) VALUES ('$section')";
+        $query_run=mysqli_query($connect, $sql);
+
+        if($query_run){
+            header('location: ../view/section.php');
+            $_SESSION['message'] = "You have successfully added a new section.";
+        }
+    }
+
+    //delete semester
+    if(isset($_GET['delete'])){
+        $section = $_GET['delete'];
+        $result = $connect->query("SELECT * FROM section WHERE sec = '$section';") or die($connect->error());
+        if(count($result) == 1){
+            $row = $result->fetch_array();
+            $id = $row['id'];   
+            $check=$connect->query("DELETE FROM section WHERE sec= '$section';") or die($connect->error);
+            if($check){
+                header('location: ../view/semester.php');
+                $_SESSION['message'] = "Successfully deleted.";
+            }else{
+                header('location: ../view/semester.php');
+                $_SESSION['message'] = "Something went wrong.";
+            }
+        }  
+    }
+
+
+?>
