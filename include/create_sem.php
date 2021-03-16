7341da0eadeb4f371f65e71cdfa987f14f392c9b
GagaracruzJanbres <j.gagaracruz@yahoo.com>
2021-03-16 23:50:40 +0800

    Update create_sem.php

--- include/create_sem.php
+++ include/create_sem.php
@@ -15,12 +15,12 @@
 
     //delete semester
     if(isset($_GET['delete'])){
-        $semester = $_GET['delete'];
-        $result = $connect->query("SELECT * FROM semester WHERE sem = '$semester';") or die($connect->error());
+        $id = $_GET['delete'];
+        $result = $connect->query("SELECT * FROM semester WHERE id = '$id';") or die($connect->error());
         if(count($result) == 1){
             $row = $result->fetch_array();
             $id = $row['id'];   
-            $check=$connect->query("DELETE FROM semester WHERE sem= '$semester';") or die($connect->error);
+            $check=$connect->query("DELETE FROM semester WHERE id= '$id';") or die($connect->error);
             if($check){
                 header('location: ../view/semester.php');
                 $_SESSION['message'] = "Successfully deleted.";
@@ -31,5 +31,25 @@
         }  
     }
 
+    if(isset($_POST['semester_id']))
+    {
+        $semester_id = $_POST['semester_id'];
+        $query = "SELECT * FROM semester WHERE sem = '$semester_id';";
+
+        $result = mysqli_query($connect,$query);
+        if(mysqli_num_rows($result) > 0){
+            echo '<i class="fa fa-times-circle text-danger ml-1"></i>                    
+                    <span p-1 class="text-danger"> 
+                        This semester is already existing.
+                    </span> ';
+            echo "<script>$('#create').prop('disabled',true);</script>"; //set disabled register button
+        }else{
+            echo  '<i class="fa fa-check-circle  text-success ml-1"></i>
+                    <span p-1" class="text-success">
+                        Semester is Available.
+                    </span>';
+            echo "<script>$('#create').prop('disabled',false);</script>"; //set enabled register button
+        }
+    }
 
 ?>
