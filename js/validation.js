120c38d62a7c3743599003ca6734ed9f0d1fe10d
GagaracruzJanbres <j.gagaracruz@yahoo.com>
2021-03-16 23:51:13 +0800

    Update validation.js

--- js/validation.js
+++ js/validation.js
@@ -1,3 +1,4 @@
+//Check the availability of email
 $(document).ready(function(){
         $("#email").blur(function(){
         var email = $("#email").val();
@@ -20,7 +21,7 @@ $(document).ready(function(){
         });   
         }                           
     });
-
+    //Check the availability of student id
     $("#student_id").blur(function(){
         var student_id = $("#student_id").val();
         if(student_id == "")
@@ -33,11 +34,100 @@ $(document).ready(function(){
             method: "POST",
             data:
             {
-                student_id:student_id
+                semester_id:semester
             },
             datatype: "text",
             success:function(html){
                 $('#student_validation').html(html);
+            }
+        });   
+        }                           
+    });
+    //Check the availability of course id
+    $("#course").blur(function(){
+        var course_id = $("#course").val();
+        if(course_id == "")
+        {
+            $('#course_validation').html("");
+            $("#create").prop('disabled', false);
+        }else{
+        $.ajax({
+            url:"../include/create_course.php",
+            method: "POST",
+            data:
+            {
+                course_check:course_id
+            },
+            datatype: "text",
+            success:function(html){
+                $('#course_validation').html(html);
+            }
+        });   
+        }                           
+    });
+    //Check the availability of semester id
+    $("#semester").blur(function(){
+        var semester_id = $("#semester").val();
+        if(semester_id == "")
+        {
+            $('#semester_validation').html("");
+            $("#create").prop('disabled', false);
+        }else{
+        $.ajax({
+            url:"../include/create_sem.php",
+            method: "POST",
+            data:
+            {
+                course_check:course_id
+            },
+            datatype: "text",
+            success:function(html){
+                $('#semester_validation').html(html);
+            }
+        });   
+        }                           
+    });
+    //Check the availability of year level id
+    $("#year_lvl").blur(function(){
+        var year_id = $("#year_lvl").val();
+        if(year_id == "")
+        {
+            $('#yearlvl_validation').html("");
+            $("#create").prop('disabled', false);
+        }else{
+        $.ajax({
+            url:"../include/create_yearlvl.php",
+            method: "POST",
+            data:
+            {
+                year_id:year_lvl
+            },
+            datatype: "text",
+            success:function(html){
+                $('#yearlvl_validation').html(html);
+            }
+        });   
+        }                           
+    });
+    //Check the availability of year school year
+    $("#current","#end").blur(function(){
+        var current_year = $("#current").val();
+        var end_year = $("#end").val();
+        if(current_year == "")
+        {
+            $('#school_validation').html("");
+            $("#save").prop('disabled', false);
+        }else{
+        $.ajax({
+            url:"../include/create_year.php",
+            method: "POST",
+            data:
+            {
+                year_id:year_lvl
+            },
+            datatype: "text",
+            success:function(html){
+                $('#school_validation').html(html);
             }
         });   
         }                           
