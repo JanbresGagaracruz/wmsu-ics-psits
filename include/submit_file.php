3566120c6b17156afa98dbec93ea6333c1a6bc2b
GagaracruzJanbres <j.gagaracruz@yahoo.com>
2021-03-23 00:00:55 +0800

    Create submit_file.php

---
+++ include/submit_file.php
@@ -0,0 +1,84 @@
+<?php
+// Include the database configuration file
+    include('database.php');
+    $statusMsg = '';
+    //check the availability of file name
+    if(isset($_POST['name']))
+    {
+        $name = $_POST['name'];
+        $query = "SELECT * FROM file_upload WHERE name = '$name';";
+
+        $result = mysqli_query($connect,$query);
+        if(mysqli_num_rows($result) > 0){
+            echo '<i class="fa fa-times-circle text-danger ml-1"></i>                    
+                    <span p-1 class="text-danger"> 
+                        This file name already existing.
+                    </span> ';
+            echo "<script>$('#create').prop('disabled',true);</script>"; //set disabled register button
+        }else{
+            echo  '<i class="fa fa-check-circle  text-success ml-1"></i>
+                    <span p-1" class="text-success">
+                        File name is Available.
+                    </span>';
+            echo "<script>$('#create').prop('disabled',false);</script>"; //set enabled register button
+        }
+    }
+    //upload a pdf file
+    if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
+        $targetDir = "../uploads/";
+        $Upload_type = array('pdf');
+        $fileName = basename($_FILES["file"]["name"]);
+        $name= $_POST['name'];
+        $path = $targetDir . $fileName;
+        $fileType = pathinfo($path,PATHINFO_EXTENSION);
+
+        if(in_array($fileType, $Upload_type )){
+            //moving the pdf file to the upload folder
+            if(move_uploaded_file($_FILES["file"]["tmp_name"], $path)){
+                $insert = $connect->query("INSERT into file_upload (name,file_name) VALUES ('".$name."','".$fileName."')");
+                if($insert){
+                    header('location: ../view/announcement.php?success=1');
+                    $_SESSION['message'] = "The file ".$fileName. " has been uploaded successfully.";
+                }else{
+                    header('location: ../view/announcement.php?success=1');
+                    $_SESSION['message'] = "File upload failed, please try again."; 
+                } 
+            }else{
+                header('location: ../view/announcement.php?success=1');
+                $_SESSION['message'] = "Sorry, there was an error uploading your file.";  
+            }
+        }else{
+            header('location: ../view/announcement.php?success=1');
+            $_SESSION['message'] = 'Sorry, only PDF file is allowed to upload.';
+        }
+    }
+
+    //delete file
+    if(isset($_GET['delete'])){
+        $id = $_GET['delete'];
+
+        $result = $connect->query("SELECT * FROM file_upload WHERE id = '$id';") or die($connect->error());
+        if(count($result) == 1){
+            $row = $result->fetch_array();
+            $id = $row['id'];  
+            $file_name = $row['file_name']; 
+            $targetDir = "../uploads/";
+            //concatenate file name and upload directory to delete the file inside the folder
+            $targetFilePath = $targetDir . $file_name;
+            $check=$connect->query("DELETE FROM file_upload WHERE id = '$id';") or die($connect->error);
+            if($check){
+                //check if the file path does exist and delete from upload folder
+                if(!unlink($targetFilePath)){
+                    header('location: ../view/announcement.php?success=1');
+                    $_SESSION['message'] = "Successfully deleted.";
+                }else{
+                    header('location: ../view/announcement.php?success=1');
+                    $_SESSION['message'] = "Successfully deleted.";
+                }
+            }else{
+                header('location: ../view/announcement.php?success=1');
+                $_SESSION['message'] = "Something went wrong.";
+            }
+        }  
+    }
+?>
