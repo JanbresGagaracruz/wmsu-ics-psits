bab942a314c875a03dfecb9cd0bf275c63322906
GagaracruzJanbres <j.gagaracruz@yahoo.com>
2021-03-26 14:16:18 +0800

    Update create_section.php

--- include/create_section.php
+++ include/create_section.php
@@ -18,12 +18,12 @@
 
     //delete semester
     if(isset($_GET['delete'])){
-        $section = $_GET['delete'];
-        $result = $connect->query("SELECT * FROM section WHERE sec = '$section';") or die($connect->error());
+        $id = $_GET['delete'];
+        $result = $connect->query("SELECT * FROM section WHERE id = '$id';") or die($connect->error());
         if(count($result) == 1){
             $row = $result->fetch_array();
             $id = $row['id'];   
-            $check=$connect->query("DELETE FROM section WHERE sec= '$section';") or die($connect->error);
+            $check=$connect->query("DELETE FROM section WHERE id= '$id';") or die($connect->error);
             if($check){
                 header('location: ../view/section.php?success=1');
                 $_SESSION['message'] = "Successfully deleted.";
@@ -34,5 +34,22 @@
         }  
     }
 
+    
+    if(isset($_POST['section']))
+    {
+        $section_id = $_POST['section'];
+        $query = "SELECT * FROM section WHERE sec = '$section_id';";
+
+        $result = mysqli_query($connect,$query);
+        if(mysqli_num_rows($result) > 0){
+            echo '<i class="fa fa-times-circle text-danger ml-1"></i>                    
+                    <span p-1 class="text-danger"> 
+                        This section is already existing.
+                    </span> ';
+            echo "<script>$('#create').prop('disabled',true);</script>"; //set disabled register button
+        }else{
+            echo "<script>$('#create').prop('disabled',false);</script>"; //set enabled register button
+        }
+    }
 
 ?>
